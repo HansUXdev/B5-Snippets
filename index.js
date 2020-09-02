d1b17c4aa76f950f31cec716fcceedfa05836918
Hans <bmm79@nau.edu>
2020-09-02 11:36:22 -0700

    add gitpod and basic server

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const fs = require('fs');
+const PORT = 7000;
+
+
+http.createServer(function (request, response) {
+  if (request.url == '/') {
+    fs.readFile('./templates/album.html', function(err, data) {
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(data);
+      return response.end();
+    });
+  }
+  else{
+    return response.end('Invalid request');
+  }
+}).listen(PORT);
